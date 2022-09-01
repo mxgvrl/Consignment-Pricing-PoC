05792cb3ad51eff0d2ff1675f96989f39b4b8dd5
bogdanov27c <zhen2ya7@yandex.ru>
2022-09-01 15:54:32 +0300

    initializing

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express();
+const port = 9229;
+app.get('/', (request, response) => {
+    response.send('Hello world!');
+});
+app.listen(port, () => console.log(`Running on port ${port}`));
