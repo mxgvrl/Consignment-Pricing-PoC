a59b3fa25e0b7b16e08296ddb6587b906b739abb
Maksim Haurylovich <mxgvrl@gmail.com>
2022-09-02 13:23:00 +0300

    feat: test lightweight endpoints

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,19 @@ import { Specification } from './types/Specification';
 const app: Express = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/specs', (req: Request, res: Response) => {
     const specifications: Specification[] = generatedSpecifications;
     res.send(specifications);
 });
 
+app.get('/hello', (req: Request, res: Response) => {
+    res.send('hello');
+});
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('default');
+});
+
 app.listen(port, () => {
-  console.log(`Server is running at https://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
